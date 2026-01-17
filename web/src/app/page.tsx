6166f018aa6ac3bb1ab1f8695e871f4012424425
SinhaWiz <sinhartx2070@gmail.com>
2026-01-17 21:59:21 +0600

    CSS updated with new animations

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -11,35 +11,3 @@ export default async function Home() {
 
   return <GameClient user={session.user} />;
 }
-
-      {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-4 py-6">
-        <div className="grid grid-cols-12 gap-6">
-          {/* Left Column - Market & Chart */}
-          <div className="col-span-8 space-y-6">
-            {/* Market Table */}
-            <MarketTable 
-              onSelectCoin={setSelectedCoin} 
-              selectedCoin={selectedCoin} 
-            />
-
-            {/* Price Chart */}
-            <PriceChart coin={selectedCoin} />
-
-            {/* Positions */}
-            <PositionsPanel />
-          </div>
-
-          {/* Right Column - Trade Panel & Actions */}
-          <div className="col-span-4 space-y-6">
-            {/* Trade Panel */}
-            <TradePanel selectedCoin={selectedCoin} />
-
-            {/* Action Panel */}
-            <ActionPanel />
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-}
